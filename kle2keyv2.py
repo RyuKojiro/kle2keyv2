8f6fadb2458acaf2262c62a22a112a60f5e9d4a1
Daniel Loffgren <daniel@loffgren.org>
2022-10-07 23:06:14 -0700

    Figure out the basename and print out a source template with the rows of layout keys and legends

--- kle2keyv2.py
+++ kle2keyv2.py
@@ -2,9 +2,37 @@
 
 import json
 import sys
+import os
 
 for arg in sys.argv[1:]:
     with open(arg) as fp:
+        basename = os.path.splitext(os.path.basename(arg))[0]
         j = json.load(fp)
         
-        print(j)
+        layout = list()
+        legends = list()
+        for row in j:
+            lay = list()
+            leg = list()
+
+            for key in row:
+                lay.append(1)
+
+            layout.append(lay)
+            legends.append(leg)
+
+        print("include <KeyV2/src/layout/layout.scad>")
+        print("")
+        print("%s_layout = [" % basename)
+        for row in layout:
+            print('\t' + str(row) + ',')
+        print("];")
+        print("")
+        print("%s_legends = [" % basename)
+        for row in legends:
+            print('\t' + str(row) + ',')
+        print("];")
+        print("")
+        print("module %s_default(profile) {" % basename)
+        print("\tlayout(%s_layout, profile, %s_legends) children();" % (basename, basename))
+        print("}")
